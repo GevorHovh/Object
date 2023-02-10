b31c74ace772cbc31f7be536e68c081b8b005db7
GevorHovh <123312885+GevorHovh@users.noreply.github.com>
2023-02-10 20:53:03 +0300

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,140 @@
+let sportZal = {
+    name: 'SHMVD',
+    age: 20,
+    tnoren: "Artash M.",
+    hark: {
+        hark1: {
+            sprortTypes: {
+                weightfiling: {
+                    treiners: {
+                        TrainerOfType1_1: {
+                            trName : "Artak",
+                            trAge: 42,
+                            qountOfHumans: 20,
+                            names: {
+                                human1: {
+                                    nameOfhuman1: "Gevor",
+                                    age: 18,
+                                    medalsOfhuman1: 10
+                                },
+                                human2: {
+                                    nameOfhuman2: "Smbat",
+                                    age1: 20,
+                                    medalsOfhuman2: 8
+                                },
+                                human3: {
+                                    nameOfhuman3: "Azat",
+                                    age3: 16,
+                                    medalsOfhuman3: 7
+                                },
+                                },
+                                
+                        },
+                        TrainerOfType1_2: {
+                            trName1: "Hovo",
+                            trAge1: 38,
+                            qountOfHumans1: 25,
+                            humansOfHovo: {
+                                humanOfHovo1: {
+                                    nameHumanOfType1_1: "Gexam",
+                                    ageHumanOfType1_1: 22,
+                                    medalsHumanOfType1_1: 11
+                                },
+                                humanOfHovo2: {
+                                    nameHumanOfType1_1: "Setrak",
+                                    ageHumanOfType1_2: 19,
+                                    medalsHumanOfType1_2: 4
+                                },
+                                humanOfHovo3: {
+                                    nameHumanOfType1_3: "Karen",
+                                    ageHumanOfType1_3: 16,
+                                    medalsHumanOfType1_3: 8
+                                },
+                            },
+                        },
+                    },
+                   
+                },
+                wrestling: {
+                    treinerOfType2: {
+                        trainerOfType2_1: {
+                            nameOfType2Tr1: "Gako",
+                            ageOfType2Tr1: 37,
+                            HumansOfType2: {
+                                qountOfHumansType2_1: 34,
+                                HumanOfType2_1: {
+                                    nameHumanOfWr1: "Soxomon",
+                                    ageHumanOfWr1: 28,
+                                    medalsHumanOfWr2: 14
+                                },
+                                HumanOfType2_2: {
+                                    nameHumanOfWr2: "Karlen",
+                                    ageHumanOfWr2: 20,
+                                    medalsHumanOfWr2: 17
+                                },
+                                HumanOfType2_3: {
+                                    nameHumanOfWr3: "Gagik",
+                                    ageHumanOfWr3: 19,
+                                    medalsHumanOfWr3: 8
+                            },
+                        },
+                        trainerOfType2_2: {
+                            nameOfType2Tr2: "Xcho",
+                            ageOfType2Tr2: 50,
+                            qountOfHumansType2: 15,
+                            humanOfWr1: {
+                                    nameHumanWrOfTr1: "Karush",
+                                    ageHumanWrOfTr1: 21,
+                                    medalsHumanWrOfTr1: 18
+                            },
+                            humanOfWr2: {
+                                nameHumanWrOfTr2: "Seryan",
+                                ageHumanWrOfTr2: 24,
+                                medalsHumanWrOfTr2: 20
+                            },
+                            humanOfWr3: {
+                                nameHumanWrOfTr3: "Levon",
+                                ageHumanWrOfTr3: 26,
+                                medalsHumanWrOfTr3: 24
+                            },
+                        },
+                    },
+                } ,
+            },
+            },
+           
+           
+        hark2: {
+            nameOfHark2: "Trinajor"
+        },
+    },
+},
+};
+
+function creatSportzal(name,
+    age,
+    tnoren,
+    hark,
+    sprortTypes,
+    treiners,
+    TrainerOfType1,
+    treinerOfType2) {
+        age,
+        name,
+        tnoren,
+        hark,
+        sprortTypes,
+        TrainerOfType1,
+        treinerOfType2
+}
+
+console.log(creatSportzal(15,"SHMVD","Artash", 2, "weightfilling","Ashot", "Hovo"));
+
+function creatTrainers(
+    trainerOfType1,
+    trainerOfType2,
+){ 
+    trainerOfType1,
+    trainerOfType2
+}
+console.log(creatTrainers("Artak", "Hovo"));
